9b96641dc6211eb77ad8fb02f9050fa98ce5f3c8
Armen Khachatryan <armen.khachatrian.int@gmail.com>
2020-06-22 11:08:24 +0400

    Add project files.

---
+++ App17/App17/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using App17.Models;
+
+namespace App17.ViewModels
+{
+  public class ItemDetailViewModel : BaseViewModel
+  {
+    public Item Item { get; set; }
+    public ItemDetailViewModel(Item item = null)
+    {
+      Title = item?.Text;
+      Item = item;
+    }
+  }
+}
